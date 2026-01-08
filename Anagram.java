120f296605aa2c66c6511d12cd7fa6667ce4c8c0
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2026-01-09 02:25:18 +0900

    Update Anagram.java

--- Anagram.java
+++ Anagram.java
@@ -19,3 +19,29 @@ class Result {
      * The function accepts STRING s as parameter.
      */
 }
+
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, q).forEach(qItr -> {
+            try {
+                String s = bufferedReader.readLine();
+
+                int result = Result.anagram(s);
+
+                bufferedWriter.write(String.valueOf(result));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
